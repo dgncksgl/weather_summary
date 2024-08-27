85b93743af861a8c7c2decbb69e72e0163fc7390
Doğancan Köseoğlu <dogancankoseoglu@gmail.com>
2024-08-27 16:42:20 +0300

    add weather api client

--- daily_news/daily_news.py
+++ daily_news/daily_news.py
@@ -1,7 +1,9 @@
 from .date_time_info import get_date_and_time_info
 from .text_to_speech_engine import init_text_to_speech_engine
+from .weather_api_client import get_weather_infos
 
 
 def get_infos():
     engine = init_text_to_speech_engine()
     get_date_and_time_info(engine)
+    get_weather_infos(engine)
--- daily_news/weather_api_client.py
+++ daily_news/weather_api_client.py
@@ -1,9 +1,23 @@
+import requests
 
-# weather info (max and min degree, current degree, feels degree, description)
-# say to goodbye
-def get_weather_infos():
-    pass
-
-
+api_key = '822ccea99e830f652ac3eec191bf60ef'
+city = 'Ankara'
+url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=tr&units=metric'
 
 
+def get_weather_infos(engine):
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        engine.say(f"{city} ili için hava bilgileri şu şekildedir")
+        engine.say(f"mevcut sıcaklık {data['main']['temp']}°C")
+        engine.say(f"hissedilen sıcaklık {data['main']['feels_like']}°C")
+        engine.say(f"hissedilen sıcaklık {data['main']['feels_like']}°C")
+        engine.say(f"maksimum sıcaklık {data['main']['temp_max']}°C")
+        engine.say(f"minimum sıcaklık {data['main']['temp_min']}°C")
+        engine.say(f"Hava {data['weather'][0]['description']}")
+    else:
+        print(f"Error {data['cod']}: {data['message']}")
+
+    engine.say("Sağlıcakla kalın")
+    engine.runAndWait()
